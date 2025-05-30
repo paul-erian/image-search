6c4cb88c4e534bdc9514ceaff17839cbc2d1bc50
Paul Erian <187680530+paul-erian@users.noreply.github.com>
2025-05-30 14:46:08 +0200

    first version of fastAPI interface

---
+++ app/main.py
@@ -0,0 +1,10 @@
+# app/main.py
+from fastapi import FastAPI, Query
+from app.clip_utils import search_images
+
+app = FastAPI()
+
+@app.get("/search")
+def search_endpoint(q: str = Query(..., description="Description de la recherche"), k: int = 5):
+    results = search_images(q, top_k=k)
+    return {"query": q, "results": [{"image_path": path, "score": score} for path, score in results]}
