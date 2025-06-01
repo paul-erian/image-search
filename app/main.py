a13a7ce7a4fa95ba930faf8a4ccf7dcd071f8415
Paul Erian <187680530+paul-erian@users.noreply.github.com>
2025-06-01 20:37:43 +0200

    add home template

--- app/main.py
+++ app/main.py
@@ -1,31 +1,18 @@
 # app/main.py
 import os
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from clip import search_images
 
 app = FastAPI()
-
 app.mount("/images", StaticFiles(directory=os.path.abspath("../images")), name="images")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <html>
-        <head>
-            <title>Image Search</title>
-        </head>
-        <body>
-            <h1>Image Search</h1>
-            <form action="/search" method="get">
-                <input type="text" name="q" placeholder="Enter your search..." required>
-                <input type="number" name="k" value="5" min="1" max="20">
-                <button type="submit">Search</button>
-            </form>
-        </body>
-    </html>
-    """
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/search", response_class=HTMLResponse)
 def search_endpoint(q: str = Query(..., description="Description de la recherche"), k: int = 5):
