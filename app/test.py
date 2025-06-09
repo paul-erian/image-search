31dc1fbc253bc20618ada118ab32ec10491dbe6e
Paul Erian <187680530+paul-erian@users.noreply.github.com>
2025-06-09 17:55:35 +0200

    remove useless file

--- app/test.py
+++
@@ -1,9 +0,0 @@
-from app.clip_utils import search_images, dowload_images_from_r2
-
-if __name__ == "__main__":
-    query = "a photo of a boat"
-    results = search_images(query, 10, 0.0)
-    image_paths_cloud = [path for path, _ in results]
-    dowload_images_from_r2(image_paths_cloud)
-    for path, score in results:
-        print(f"{path}:{score}")
