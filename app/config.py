ee4860a1e9837cbc7314d3de228b2293f05a2b39
Paul Erian <187680530+paul-erian@users.noreply.github.com>
2025-06-09 16:05:56 +0200

    put paths in config file

--- app/config.py
+++ app/config.py
@@ -2,12 +2,21 @@ import os
 import boto3
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="../.env")
+# chemins distant
+s3_embeddings_dir = "embeddings"
+model_name = "openai/clip-vit-base-patch32"
 
+# chemins locaux
+embeddings_dir = "../embeddings"
+embeddings_name = "clip_embeddings_20000.pt"
+clip_path = "./clip_model"
+image_dir = "../images"
+env_path = "../.env"
+
+# client s3
+load_dotenv(dotenv_path=env_path)
 bucket = os.getenv("BUCKET_NAME")
-
-session = boto3.session.Session()
-s3 = session.client(
+s3 = boto3.session.Session().client(
     service_name='s3',
     endpoint_url=os.getenv("ENDPOINT_URL"),
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
--- app/main.py
+++ app/main.py
@@ -6,29 +6,29 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from clip_utils import load_clip
-from config import s3, bucket
+from config import s3_embeddings_dir, model_name, embeddings_dir, embeddings_name, clip_path, image_dir, s3, bucket
 from s3_data_downloader import S3DataDownloader
 from clip_image_searcher import ClipImageSearcher
 
 print("telechargement des embeddings image depuis s3 ...")
 downloader = S3DataDownloader(s3, bucket)
-downloader.download(["embeddings/clip_embeddings.pt"], "../embeddings")
+downloader.download([os.path.join(s3_embeddings_dir, embeddings_name)], embeddings_dir)
 
 print("chargement des embeddings image dans la RAM ...")
-embeddings = torch.load("../embeddings/clip_embeddings_20000.pt")
+embeddings = torch.load(os.path.join(embeddings_dir, embeddings_name))
 s3_image_paths = list(embeddings.keys())
 image_features = torch.stack(list(embeddings.values())).half()
 del embeddings
 
 print("chargement clip ...")
-model, processor = load_clip(model_name="openai/clip-vit-base-patch32", local_dir="./clip_model")
+model, processor = load_clip(model_name=model_name, local_dir=clip_path)
 clip_image_searcher = ClipImageSearcher(model=model,
                                         processor=processor,
                                         image_features=image_features,
                                         s3_image_paths=s3_image_paths)
 
 app = FastAPI()
-app.mount("/images", StaticFiles(directory=os.path.abspath("../images")), name="images")
+app.mount("/images", StaticFiles(directory=os.path.abspath(image_dir)), name="images")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
@@ -39,7 +39,7 @@ def home(request: Request):
 def search_endpoint(request: Request, q: str = Query(..., description="Description de la recherche"), k: int = 12, t: float = 0.2):
     results = clip_image_searcher.search(q, top_k=k, treshold=t)
     s3_images_to_donwload = [path for path, _ in results]
-    downloader.download(s3_images_to_donwload, "../images")
+    downloader.download(s3_images_to_donwload, image_dir)
     return templates.TemplateResponse("results.html", {
         "request": request,
         "q": q,
