557165321c5d2f1a9e95cd7e40bcf395bcde3c0a
paul-erian <erianpaulsevan@gmail.com>
2026-04-12 14:57:18 +0200

    add compute embedding and directory flatten scripts

---
+++ scripts/compute_embeddings.py
@@ -0,0 +1,47 @@
+import argparse
+from pathlib import Path
+
+import torch
+from PIL import Image
+from torch.nn.functional import normalize
+from tqdm import tqdm
+from transformers import CLIPModel, CLIPProcessor
+
+
+def compute_embeddings(image_dir: Path, image_format: str)->None:
+    """Calcule les embeddings CLIP pour les images d'un répertoire
+       et les sauvegarde dans un fichier .pt."""
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Chargement du modèle CLIP
+    model_name = "openai/clip-vit-base-patch32"
+    model = CLIPModel.from_pretrained(model_name).to(device)
+    processor = CLIPProcessor.from_pretrained(model_name)
+
+    # Chemins
+    input_dir = Path(image_dir)
+    image_paths = list(input_dir.rglob("*." + image_format))
+    output_path = input_dir / "clip_embeddings.pt"
+
+    # Calcul des embeddings
+    embeddings = {}
+    for image_path in tqdm(image_paths, desc="Computing embeddings"):
+        image = Image.open(image_path).convert("RGB")
+        inputs = processor(images=image, return_tensors="pt", padding=True).to(device)
+
+        with torch.no_grad():
+            image_features = model.get_image_features(**inputs)
+            image_features = image_features.pooler_output # (batch_size, embedding_dim)
+            image_features = normalize(image_features, p=2, dim=-1)
+            embeddings[image_path.name] = image_features.cpu().numpy()
+    
+    torch.save(embeddings, output_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_dir', type=Path, help='Répertoire contenant les images')
+    parser.add_argument('--image_format', type=str, default="JPEG", help='Format des images')
+    args = parser.parse_args()
+
+    compute_embeddings(args.image_dir, args.image_format)
